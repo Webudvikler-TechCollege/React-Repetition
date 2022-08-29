3c44d855ed6fe04855978f071392bc4e6aa5d8f8
Heinz K <heka@techcollege.dk>
2022-08-29 19:13:11 +0200

    Auth Update

--- stringsonline/src/Components/App/Auth/Auth.jsx
+++ stringsonline/src/Components/App/Auth/Auth.jsx
@@ -1,16 +1,30 @@
+/**
+ * Følgende kode laver et komponent (<AuthProvider>) som kan 
+ * transportere data ned i gennem reacts komponent hieraki.
+ * Det betyder at vi nemt kan hente en brugers logindata fra alle 
+ * child components. Derfor skal AuthProvider komponentet placeres 
+ * så højt som muligt i hierakiet - eksempelvis i filen /src/index.js
+ * Filens custom hook (useAuth) bruges desuden på Login siden.
+ */
 import { createContext, useContext, useEffect, useState } from "react";
 
+// Initialiserer auth context
 const AuthContext = createContext();
 
+// Definerer Contekst Provider
+// med props.children som tilstandsværdi
 const AuthProvider = ({ children }) => {
 	const [loginData, setLoginData] = useState('')
 
+	// Opdater loginData med data fra sessionstorage hvis det findes
 	useEffect(() => {
 		if(sessionStorage.getItem('token')) {
 			setLoginData(JSON.parse(sessionStorage.getItem('token')))
 		}
 	}, [children])
 
+	// Returner provider
+	// Alle childs af denne får adgang til logindata
 	return (
 		<AuthContext.Provider value={{loginData,setLoginData}}>
 			{children}
@@ -18,6 +32,9 @@ const AuthProvider = ({ children }) => {
 	)
 }
 
+// Definerer context hook
+// Gør at vi nemt kan kalde context
+// Eksempel: const { loginData } = useAuth()
 const useAuth = () => useContext(AuthContext);
 
 export { AuthContext, AuthProvider, useAuth }
