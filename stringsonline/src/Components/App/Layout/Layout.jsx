46292264a9af5e230b755c269626037ff8434f94
Heinz K <heka@techcollege.dk>
2022-08-29 19:16:47 +0200

    Layout comments

--- stringsonline/src/Components/App/Layout/Layout.jsx
+++ stringsonline/src/Components/App/Layout/Layout.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react"
 
+// Function Component til side hoved layout
 const Layout = props => {
+	// useEffect renderer når title eller description skifter værdi
 	useEffect(() => {
+		// Sætter dokumentets titel
 		document.title = props.title
+		// Hvis description er sat på props objekt
 		if(props.description) {
+			// Sæt description i dom'en
 			document.querySelector('meta[name="description"]')
 				.setAttribute('content', props.description)
 		}
+		// Dependency array overvåger title og description
 	}, [props.title, props.description])
 
 	return (
