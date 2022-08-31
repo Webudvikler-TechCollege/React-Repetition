dde6d70755c9552fbedb841c59a747d705980c46
Heinz K <heka@techcollege.dk>
2022-08-31 07:33:30 +0200

    Layout & SCSS updates

--- stringsonline/src/App.jsx
+++ stringsonline/src/App.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter } from 'react-router-dom'
-import './App.scss';
-import { AppRouter } from './Components/App/Router/Router';
-import { Footer } from './Components/Partials/Footer';
-import { Header } from './Components/Partials/Header';
-import { Main } from './Components/Partials/Main';
-import { NavBar } from './Components/Partials/NavBar';
-import { NavBarLeft } from './Components/Partials/NavBarLeft';
+import { BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import { AppRouter } from "./Components/App/Router/Router";
+import { Footer } from "./Components/Partials/Footer";
+import { Header } from "./Components/Partials/Header";
+import { Main } from "./Components/Partials/Main";
+import { NavBarLeft } from "./Components/Partials/NavBarLeft";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <NavBar />
-      <Main>
-        <NavBarLeft />
-        <AppRouter />
-      </Main>
-      <Footer />
+      {/* Page Container til at centrere sitet */}
+      <div className="pageContainer">
+        <Header />
+        <Main>
+          <NavBarLeft />
+          <AppRouter />
+        </Main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
