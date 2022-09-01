f7923e06ab254004038875661bd7e0aeda133b4c
Heinz K <heka@techcollege.dk>
2022-09-01 12:10:03 +0200

    Route Update - Products Component

---
+++ stringsonline/src/Components/Pages/Products/Products.jsx
@@ -0,0 +1,9 @@
+import { Layout } from "../../App/Layout/Layout"
+
+export const Products = () => {
+	return (
+		<Layout title="Produkter" description="Se vores udvalg af instrumenter">
+			<p>Se vores udvalg af instrumenter</p>
+		</Layout>
+	)
+}
