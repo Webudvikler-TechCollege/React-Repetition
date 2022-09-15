bc7868691fbf02e7b648f40c6ef18750b1d41a0b
Heinz K <heka@techcollege.dk>
2022-09-15 16:52:26 +0200

    FAVORITKNAP DER VIRKER!!! :D

--- react-snippets/src/Components/App/Favorites/FavoriteButton.jsx
+++ react-snippets/src/Components/App/Favorites/FavoriteButton.jsx
@@ -1,61 +1,72 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useAuth } from '../../App/Auth/Auth'
-import { AiOutlineHeart } from 'react-icons/ai'
-import Styles from './FavoriteButton.module.scss'
-import { useFavorites } from '../Context/FavoriteProvider'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useAuth } from "../../App/Auth/Auth"
+import { AiOutlineHeart } from "react-icons/ai"
+import Styles from "./FavoriteButton.module.scss"
+import { useFavorites } from "../Context/FavoriteProvider"
 
+/**
+ * Knap komponent som kan tilføje og fjerne et 
+ * produkt fra en favorit liste.
+ * Anvender FavoriteProvider med useContext metode
+ * @param {*} param 
+ * @returns 
+ */
 const FavoriteButton = ({ product_id }) => {
-	const { loginData } = useAuth()
-	const { favorites } = useFavorites()
-	const [ isFavorite, setIsFavorite ] = useState(false)
+  // Henter auth data
+  const { loginData } = useAuth()
+  // Henter liste over eksisterende favoritter
+  const { favorites } = useFavorites()
+  // Sætter var til at indikere om produkt er favorit eller ej
+  const [isFavorite, setIsFavorite] = useState(false)
 
-	/**
-	 * Object var til api info
-	 */
-	const api_info = {
-		endpoint: 'https://api.mediehuset.net/snippets/favorites',
-		options: { headers: {
-			Authorization: `Bearer ${loginData.access_token}`
-		}}
-	}
+  // Kalder useEffect
+  useEffect(() => {
+	// Hvis vi allerede har favoritter...
+    if(favorites.length) {
+	  // Setter bool efter om produkt ligger i listen over favoritter
+      setIsFavorite(() =>
+        favorites.some((item) => item.product_id === product_id)
+      )
+    }
+  }, [favorites, product_id])
 
-	/**
-	 * UseEffect Hook til styring af favorit status ud fra product_id
-	 */
-	useEffect(() => {
-		if(favorites && product_id) {
-			setIsFavorite(favorites.some(item => item.product_id === product_id))
-			console.log(product_id, isFavorite);
-		}
-	}, [favorites, isFavorite, product_id])
+  // Toggle funktion 
+  const toggleFavorite = async () => {
+	// Endpoint path
+    const endpoint = "https://api.mediehuset.net/snippets/favorites"
+	// Header options med token key
+    const options = {
+      headers: {
+        Authorization: `Bearer ${loginData.access_token}`,
+      },
+    }
 
-	/**
-	 * Tilføj favorit
-	 */
-	const addFavorite = async () => {
-		setIsFavorite(true)
+	// Hvis favorit er true
+    if (isFavorite) {
+	  // Delete request til api endpoint
+	  await axios.delete(`${endpoint}/${product_id}`, options)
+      setIsFavorite(false)
+    } else {
+	  // Sætter form data
+      const formData = new FormData()
+      formData.append("product_id", product_id)
+	  // Create kald til API endpoint
+      await axios.post(endpoint, formData, options)
+      setIsFavorite(true)
+    }
+  }
 
-		const formData = new FormData();
-		formData.append('product_id',product_id)
-		await axios.post(api_info.endpoint, formData, api_info.options)		
-	}
-
-	/**
-	 * Fjern favorit
-	 */
-	const removeFavorite = async () => {
-		setIsFavorite(false)
-		await axios.delete(`${api_info.endpoint}/${product_id}`, api_info.options)
-	}
-
-	return (
-		<button 
-			onClick={!isFavorite ? addFavorite : removeFavorite} 
-			className={isFavorite ? `${Styles.button} ${Styles.active}` : Styles.button}>
-			<AiOutlineHeart />
-		</button>
-	);
+  return (
+    <>
+      <button
+        onClick={toggleFavorite}
+        className={
+          isFavorite ? `${Styles.button} ${Styles.active}` : Styles.button
+        }
+      ><AiOutlineHeart /></button>
+    </>
+  )
 }
 
-export default FavoriteButton;
+export default FavoriteButton
