f97b9af369a908f31cc98569397ac1213f21469d
Heinz K <heka@techcollege.dk>
2022-09-15 10:02:53 +0200

    Cart Updates

--- react-snippets/src/Components/App/Cart/AddToCart.jsx
+++ react-snippets/src/Components/App/Cart/AddToCart.jsx
@@ -1,34 +1,63 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { useForm } from 'react-hook-form'
 import { useAuth } from '../Auth/Auth'
 import { useCart } from '../Context/CartProvider'
 
 const CartButton = ({product_id}) => {
 	const { loginData } = useAuth()
 	const { cartLines } = useCart();
-	const [ quantityInCart, setQuantityInCart ] = useState(1)
-	const { register, handleSubmit } = useForm()
+	const [ quantityInCart, setQuantityInCart ] = useState(0)
+
+	const endpoint = 'https://api.mediehuset.net/snippets/cart'
+	const options = {
+		headers: {
+			Authorization: `Bearer ${loginData.access_token}`
+		}
+	}
 
 	useEffect(() => {
+	
 		if(cartLines) {
 			const objInCart = cartLines.find(item => item.product_id === product_id);
 		
 			if(objInCart) {
 				setQuantityInCart(objInCart.quantity);
 			}	
-		}
+		}		
 	}, [cartLines, product_id, quantityInCart])
 
-	const handleCart = async (data, e) => {
-		console.log(cartLines);
+
+
+	const decreaseCartItem = () => {
+		console.log(parseInt(quantityInCart)-1);
+	}
+
+	const increaseCartItem = async () => {
+		const newQuantity = parseInt(quantityInCart)+1
+		//console.log(product_id, newQuantity)
+		const urlParams = new URLSearchParams()
+		urlParams.append('product_id', product_id)
+		urlParams.append('quantity', newQuantity)
+		console.log(...urlParams);
+		if(quantityInCart) {
+			const result = await axios.put(endpoint, urlParams, options)
+			console.log(result.data.id)
+		} else {
+			const result = await axios.post(endpoint, urlParams, options)
+			console.log(result.data.id)
+		}
+		setQuantityInCart(newQuantity)
+		
 	}
 	
+	
+
 	return (
-		<form onSubmit={handleSubmit(handleCart)}>
-			<input type="hidden" defaultValue={product_id} {...register('product_id')} />
-			<input type="number" value={quantityInCart} {...register('quantity', { required: true })}></input>
-			<button>Læg i kurv</button>
+		<form>
+			<input type="hidden" name="" value={product_id} />
+			<button type="button" onClick={() => decreaseCartItem}>-</button>
+			<input type="number" disabled value={(quantityInCart) ? quantityInCart : 0}></input>
+			<button type="button" onClick={increaseCartItem}>+</button>
 		</form>
 	)
 }
